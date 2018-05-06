3b1373703ee5080e884b62c5074564a6a3fa0847
laidu <laidu823@gmail.com>
2018-05-07 00:02:10 +0800

    sync

--- init_env.sh
+++ init_env.sh
@@ -7,20 +7,24 @@
 #    usage :
 #            1. sh init_env.sh
 
-#npm install -g hexo-cli
+
 npm install hexo --save
 
 npm install hexo-migrator-wordpress --save
 npm install hexo-generator-feed --save
-#npm install hexo-generator-baidu-sitemap --save
+
 npm install hexo-renderer-marked --save
 npm install hexo-generator-search --save
+
 #自动为站外链接添加nofollow属性
 npm install hexo-autonofollow --save
+
 #生成博客的sitemap。
 npm install hexo-generator-sitemap --save
+
 #用于生成静态站点数据，提供搜索功能的数据源。
 npm install hexo-generator-json-content --save
+
 #为文章添加文章字数统计、文章预计阅读时间
 #通过以上安装后，你可以在你的模板文件加入以下相关的标签实现本插件的功能
 #**字数统计:**WordCount
@@ -31,3 +35,13 @@ npm install hexo-wordcount --save
 npm install hexo-generator-restful --save
 npm install hexo-renderer-marked --save
 npm install hexo-renderer-stylus --save
+
+## rss插件
+npm install hexo-generator-feed --save
+## 站点sitemap生成插件
+npm install hexo-generator-sitemap --save
+npm install hexo-generator-baidu-sitemap --save
+## 百度url提交
+npm install hexo-baidu-url-submit --save
+## 本地搜索插件集成
+npm install hexo-generator-search --save
